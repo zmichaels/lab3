85b5b5a83e779178e8b7698dfcd9d8914be486f9
zmichaels <31547240+zmichaels@users.noreply.github.com>
2017-09-26 16:01:07 -0700

    test

---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public int findParentheses(String str){
+        for(int i = 0; i <= str.size; i++ ) {
+            if(str.charAt(i) != '(' || str.charAt(i) != '{' || str.charAt(i) != '[') {
+                
+            }
+
+        }
+
+     return True;
+    }
+
+
+
+}
